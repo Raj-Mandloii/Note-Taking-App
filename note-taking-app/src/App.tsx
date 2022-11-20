8bd759ba734685a6c2f40afcc6dfca04d725bbb3
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-11-20 15:04:55 +0530

    useLocal storage hook created

--- note-taking-app/src/App.tsx
+++ note-taking-app/src/App.tsx
@@ -2,10 +2,34 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
 import { NewNote } from "./NewNote"
+import useLocalStorage from "./useLocalStorage"
 
+export type RawNote = {
+  id:string
+
+}
+export type RawNoteData = {
+  title: string,
+  markdown: string,
+  tagIds: string[]
+}
+export type Note = {
+  id: string
+} & NoteData
+export type NoteData = {
+  title: string,
+  markdown: string,
+  tags: Tag[]
+}
+export type Tag = {
+  id: string,
+  label: string
+}
 function App() {
+  const [notes,setNotes] = useLocalStorage<RawNote[]>("NOTES",[])
+  const [tags,setTags] = useLocalStorage<Tag[]>("TAGS",[])
 
-
+  
   return (
     <Container className="my-4">
 
@@ -13,7 +37,7 @@ function App() {
 
         <Route path="/" element={<h1>Home</h1>} />
 
-        <Route path="/new" element={<NewNote/>} />
+        <Route path="/new" element={<NewNote />} />
 
         <Route path="/:id">
 
--- note-taking-app/src/NoteForm.tsx
+++ note-taking-app/src/NoteForm.tsx
@@ -1,32 +1,68 @@
-import { Form, Stack, Row, Col,Button } from "react-bootstrap";
+import { useRef, useState } from "react";
+import { Form, Stack, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable"
-export const NoteForm = () => {
-    return <Form>
-        <Stack>
+import { NoteData, Tag } from "./App";
+
+type NoteFormProps = {
+    onSubmit: (data: NoteData) => void
+}
+export const NoteForm = ({ onSubmit }: NoteFormProps) => {
+    const titleRef = useRef<HTMLInputElement>(null)
+    const markdownRef = useRef<HTMLTextAreaElement>(null);
+    const [selectedTag, setSelectedTag] = useState<Tag[]>([])
+    // function handleSubmit (e: FormEvent) {
+    function handleSubmit(e: any) {
+        e.preventDefault();
+        onSubmit({
+            title: titleRef.current!.value,
+            markdown: markdownRef.current!.value,
+            tags: [],
+        });
+    }
+    return <Form onSubmit={handleSubmit}>
+        <Stack gap={4}>
             <Row>
                 <Col>
                     <Form.Group controlId="title">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control required />
+                        <Form.Control ref={titleRef} required />
 
                     </Form.Group>
                 </Col>
                 <Col>
                     <Form.Group controlId="tags">
                         <Form.Label>Tags</Form.Label>
-                        <CreatableReactSelect isMulti />
+                        <CreatableReactSelect
+                            value={selectedTag.map(tag => {
+                                return {
+                                    label: tag.label,
+                                    value: tag.id
+                                }
+                            })}
+                            onChange={tags => {
+                                setSelectedTag(tags.map(tag => {
+                                    return {
+                                        label: tag.label,
+                                        id: tag.value
+                                    }
+                                }))
+                            }}
+                            isMulti />
 
                     </Form.Group>
                 </Col>
             </Row>
             <Form.Group controlId="markdown">
                 <Form.Label>Body</Form.Label>
-                <Form.Control required as="textarea" rows={15} />
+                <Form.Control ref={markdownRef} required as="textarea" rows={15} />
 
             </Form.Group>
-            <Stack direction="horizontal">
+            <Stack direction="horizontal" gap={2} className="justify-content-end">
                 <Button type="submit" variant="primary">Save</Button>
-                <Button type="button" variant="outline-secondary">Cancel</Button>
+                <Link to="..">
+                    <Button type="button" variant="outline-secondary">Cancel</Button>
+                </Link>
             </Stack>
         </Stack>
     </Form>
